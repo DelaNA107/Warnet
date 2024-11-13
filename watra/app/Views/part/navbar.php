787c8d14d008f1238f049f31477992cea1fa0ef5
febywidya <feby.21122016@mahasiswa.unikom.ac.id>
2024-11-13 10:16:39 +0700

    UpdateNavbarFeby.php

--- watra/app/Views/part/navbar.php
+++ watra/app/Views/part/navbar.php
@@ -41,6 +41,7 @@
                                 </div>
                             </div>
                             <a href="contact.html" class="nav-item nav-link">Intan</a>
+                            <a href="contact.html" class="nav-item nav-link">Feby</a>
                         </div>
                         <div class="navbar-nav ml-auto py-0">
                             <a href="" class="nav-item nav-link">Login</a>
